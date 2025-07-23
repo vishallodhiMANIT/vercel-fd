780b374b47c44ece01ecc78e7663db72a76a702a
vishallodhiMANIT <vishallodhi896@gmail.com>
2025-07-23 18:41:39 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://verbal-bd-1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
